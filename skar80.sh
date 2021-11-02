b12c215c8be0bd540d23f8391b4ba4fa32edf374
skar-hack80 <93617913+skar-hack80@users.noreply.github.com>
2021-11-02 23:46:56 +0300

    Add files via upload

---
+++ skar80.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+clear
+echo hi  me ahmd  And this is the basic download tool Termu
+echo me ahmd  And this is the basic download tool Termux
+echo
+echo We start by installing the basics.
+echo My social communication accounts
+echo ########################################################
+toilet -F gay "skar.80"
+
+echo  inst:0ham1
+echo   Telegram:z0hma
+echo YouTube:https://youtube.com/channel/UC9lZ1luKm9i5986DHmbLG9w
+echo #############################################################
+apt update && apt upgrade -y
+pkg install git -y
+pkg install python -y
+pkg install python2 -y
+pkg install figlet -y
+pkg install wget -y
+pkg install root-repo
+pkg install unstable-repo
+pkg install x11-repo
+pkg install ruby -y
+pkg install nano -y
+pkg install toilet -y
+pkg install locate -y
+gem install figlet
+pip install wordlist
+pkg install termux-api -y
+pkg install ncurses-utils -y
+pip install youtube-dl
+apt install golang -y
+apt install php -y
+apt install cmatrix -y
+pkg install cowsay -y
+pkg install ruby -y
+pkg install openssh -y
+
+termux-setup-storage -y
+pkg install unzip -y
+apt install cmatrix  -y
+pkg install nmap -y
+pkg install tor -y
+pip2 install requests -y
+pkg install proot -y
+pkg install figlet -y
+apt install golang -y
+##########################################
+#########################################
+              toilet -F gay "The end"
+
+
+
+
+
+
